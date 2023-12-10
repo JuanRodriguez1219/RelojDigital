3385a7779ca1262922ed6dca6de3c23712ef04d3
JuanRodriguez1219 <110642261+JuanRodriguez1219@users.noreply.github.com>
2023-12-10 17:50:08 -0500

    Add files via upload

---
+++ Proyecto 2 Reloj Digital/relojdigital/scripts.js
@@ -0,0 +1,17 @@
+function calcularTiempo(){
+    let tiempo = new Date();
+    let hora = tiempo.getHours()
+    let minutos = tiempo.getMinutes()
+    let segundos = tiempo.getSeconds()
+
+    hora = hora < 10 ? "0" + hora : hora;
+    minutos = minutos < 10 ? "0" + minutos : minutos;
+    segundos = segundos < 10 ? "0" + segundos : segundos;
+
+    let pantallaReloj = hora + ":" + minutos + ":" + segundos;
+    let relojdigital = document.querySelector(".reloj");
+    relojdigital.innerHTML = pantallaReloj
+}
+
+
+setInterval(calcularTiempo, 1000);
